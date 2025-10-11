5a1f3c94cad131a453ad8578431b7b1074569a0c
AngelScorpio <ageepb@gmail.com>
2025-10-10 21:22:57 -0500

    primer commit de angee prueba 2

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    prueba = " Prueba angee"
-    return "Hola desde Flask en Azure con Git Hub - FUNCIONA !!! - 2025-10-10 18:31:00" + prueba
+    return "Hola desde Flask en Azure con Git Hub - FUNCIONA !!! - 2025-10-10 18:31:00 -gee"
